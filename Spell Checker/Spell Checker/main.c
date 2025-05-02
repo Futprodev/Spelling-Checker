795a0e75ad27311c1665ca2bd0a66f2e0ab5eaf9
Meru:3 <maulanaammar101@gmail.com>
2025-05-02 07:32:52 +0700

    fixed missing render

--- Spell Checker/Spell Checker/main.c
+++ Spell Checker/Spell Checker/main.c
@@ -16,6 +16,28 @@ typedef Uint64 u64;
 typedef Sint32 i32;
 typedef Sint64 i64;
 
+typedef struct
+{
+	int x;
+	int y;
+	int w;
+	int h;
+} rect_t;
+
+
+void FillRect(rect_t rect, u32 pixel_color, u32 *screen_pixels)
+{
+	SDL_assert(screen_pixels);
+
+	for (int row = 0; row < rect.h; ++row)
+	{
+		for (int col = 0; col < rect.w; ++col)
+		{
+			screen_pixels[(row + rect.y)*SCREEN_WIDTH + col + rect.x] = pixel_color;
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -24,20 +46,28 @@ int main(int argc, char* argv[])
 										SCREEN_WIDTH,
 										SCREEN_HEIGHT,
 										SDL_WINDOW_RESIZABLE);
-	assert(win);
+	SDL_assert(win);
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(win, 0);
-	assert(renderer);
+	SDL_assert(renderer);
 
 	SDL_Texture* screen = SDL_CreateTexture(renderer,
 											SDL_PIXELFORMAT_XRGB8888,
 											SDL_TEXTUREACCESS_STREAMING,
 											SCREEN_WIDTH,
 											SCREEN_HEIGHT);
-	assert(screen);
+	SDL_assert(screen);
 
 	u32 *screen_pixels = (u32*)calloc(SCREEN_WIDTH * SCREEN_HEIGHT, sizeof(u32));
-	assert(screen_pixels);
+	SDL_assert(screen_pixels);
+
+	SDL_PixelFormat *format = SDL_GetPixelFormatDetails(SDL_PIXELFORMAT_XRGB8888);
+	SDL_assert(format);
+
+	rect_t square = { 0, 0, 30, 30 };
+	u32 pixel_color = SDL_MapRGB(format, NULL, 0, 0, 255);
+	FillRect(square, pixel_color, screen_pixels);
+
 
 	BOOL done = FALSE;
 
@@ -67,13 +97,15 @@ int main(int argc, char* argv[])
 			default:
 				break;
 			}
-
-			SDL_UpdateTexture(screen, NULL, screen_pixels, NULL);
-			SDL_RenderClear(renderer);
-			SDL_RenderTexture(renderer, screen, NULL, NULL);
-			SDL_RenderPresent(renderer);
-
-			SDL_Delay(40);
 		}
+
+		SDL_UpdateTexture(screen, NULL, screen_pixels, SCREEN_WIDTH * sizeof(u32));
+		SDL_RenderClear(renderer);
+		SDL_RenderTexture(renderer, screen, NULL, NULL);
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(14);
 	}
+
+	return 0;
 }
