003c55f190a28d0bfd3e518c46dfbabafcc95866
Meru:3 <maulanaammar101@gmail.com>
2025-05-02 22:07:39 +0700

    cursor movement

--- Spell Checker/Spell Checker/main.c
+++ Spell Checker/Spell Checker/main.c
@@ -42,38 +42,46 @@ int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_Window* win = SDL_CreateWindow("Spell Checker",
+	SDL_Window *win = SDL_CreateWindow("Spell Checker",
 										SCREEN_WIDTH,
 										SCREEN_HEIGHT,
 										SDL_WINDOW_RESIZABLE);
 	SDL_assert(win);
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(win, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(win, 0);
 	SDL_assert(renderer);
 
-	SDL_Texture* screen = SDL_CreateTexture(renderer,
+	SDL_Texture *screen = SDL_CreateTexture(renderer,
 											SDL_PIXELFORMAT_XRGB8888,
 											SDL_TEXTUREACCESS_STREAMING,
 											SCREEN_WIDTH,
 											SCREEN_HEIGHT);
 	SDL_assert(screen);
 
-	u32 *screen_pixels = (u32*)calloc(SCREEN_WIDTH * SCREEN_HEIGHT, sizeof(u32));
+	u32 *screen_pixels = (u32*) calloc(SCREEN_WIDTH * SCREEN_HEIGHT, sizeof(u32));
 	SDL_assert(screen_pixels);
 
 	SDL_PixelFormat *format = SDL_GetPixelFormatDetails(SDL_PIXELFORMAT_XRGB8888);
 	SDL_assert(format);
 
 	rect_t square = { 0, 0, 30, 30 };
-	u32 pixel_color = SDL_MapRGB(format, NULL, 0, 0, 255);
+	u32 pixel_color = SDL_MapRGB(format, NULL, 0, 255, 0);
+	square.x = (SCREEN_WIDTH - square.w)/2;
+	square.y = (SCREEN_HEIGHT - square.h)/2;
 	FillRect(square, pixel_color, screen_pixels);
 
 
 	BOOL done = FALSE;
 
+	BOOL pressed_up = FALSE;
+	BOOL pressed_down = FALSE;
+	BOOL pressed_left = FALSE;
+	BOOL pressed_right = FALSE;
+
 	while (!done)
 	{
 		SDL_Event event;
+
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_Quit)
@@ -94,10 +102,54 @@ int main(int argc, char* argv[])
 			case SDLK_ESCAPE:
 				done = TRUE;
 				break;
+			case SDLK_UP:
+				pressed_up = TRUE;
+				break;
+			case SDLK_DOWN:
+				pressed_down = TRUE;
+				break;
+			case SDLK_LEFT:
+				pressed_left = TRUE;
+				break;
+			case SDLK_RIGHT:
+				pressed_right = TRUE;
+				break;
 			default:
 				break;
 			}
 		}
+
+		memset(screen_pixels, 0,SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(u32));
+
+		if (pressed_up)
+		{
+			square.y -= 10;
+			if (square.y < 0)
+			{
+				square.y += 0;
+			}
+			pressed_up = FALSE;
+		} 
+		
+		if (pressed_down)
+		{
+			square.y += 10;
+			pressed_down = FALSE;
+		}
+
+		if (pressed_left)
+		{
+			square.x -= 10;
+			pressed_left = FALSE;
+		}
+
+		if (pressed_right)
+		{
+			square.x += 10;
+			pressed_right = FALSE;
+		}
+
+		FillRect(square, pixel_color, screen_pixels);
 
 		SDL_UpdateTexture(screen, NULL, screen_pixels, SCREEN_WIDTH * sizeof(u32));
 		SDL_RenderClear(renderer);
